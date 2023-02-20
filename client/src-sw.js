c920d4da58fcf65b0edfe8a621ba8b3039fb2afd
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-02-20 22:19:05 +0000

    install.js complete

--- client/src-sw.js
+++ client/src-sw.js
@@ -38,7 +38,7 @@ registerRoute(
        }),
        new ExpirationPlugin({
          maxEntries: 60,
-         maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+         maxAgeSeconds: 30 * 24 * 60 * 60, 
        })
      ],
    })
