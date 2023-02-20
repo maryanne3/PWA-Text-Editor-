0516a0af02820c5e16ba82c34ff9102e5767eef5
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-02-20 14:49:19 +0000

    Implement asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+   // TODO: Callback function 
+   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+   new StaleWhileRevalidate({
+     cacheName: 'asset-cache',
+     plugins: [
+       new CacheableResponsePlugin({
+         statuses: [0, 200],
+       }),
+       new ExpirationPlugin({
+         maxEntries: 60,
+         maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+       })
+     ],
+   })
+);
