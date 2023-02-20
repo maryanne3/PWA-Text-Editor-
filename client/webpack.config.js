6032beb35004ab2bb7efcfc60c9754276ca13147
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-02-20 14:18:16 +0000

    Add in plugins, css loaders, & babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,63 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker 
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+        }),
+        // TODO: Add a manifest.json file.
+      new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: 'PWA-Text-Editor',
+          short_name: 'JATE',
+          description: 'PWA-Text-Editor',
+          background_color: '#225ca3',
+          theme_color: '#225ca3',
+          start_url: '/',
+          publicPath: '/',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
+          ],
+        }),
     ],
 
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+    
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+       
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
